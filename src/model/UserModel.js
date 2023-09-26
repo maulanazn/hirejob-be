8d8eb8d0a3a940c3b35665babcd84fd0a1a7a6ac
selemene112 <ariyanda98y@gmail.com>
2023-09-26 16:46:20 +0800

    Fix View all Candidate Make new Function on model for all candidate

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -3,23 +3,36 @@ const { v4: uuidv4 } = require('uuid');
 
 const getAllUserModel = async (data) => {
   try {
-    const result = await pool.query(`SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE ${data.sortBy} ILIKE '%${data.sort}%' OFFSET ${data.offset} LIMIT ${data.limit}`);
+    const result = await pool.query(
+      `SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE ${data.sortBy} ILIKE '%${data.sort}%' OFFSET ${data.offset} LIMIT ${data.limit}`
+    );
 
     return result;
   } catch (error) {
     throw new Error(error.message);
   }
-}
+};
+
+const GetallCandidateModel = async (data) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
 const searchAllUserModel = async (data) => {
   try {
-    const result = await pool.query(`SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE name ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE last_work ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE province ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE city ILIKE '%${data.search}%'`);
+    const result = await pool.query(
+      `SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE name ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE last_work ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE province ILIKE '%${data.search}%' UNION SELECT candidate_profile.photo AS candidate_photo, name, last_work, province, city FROM users JOIN candidate_profile ON users.id = users.id WHERE city ILIKE '%${data.search}%'`
+    );
 
     return result;
   } catch (error) {
     throw new Error(error.message);
   }
-}
+};
 
 const createUserModel = async (body) => {
   const id = uuidv4();
@@ -33,30 +46,39 @@ const createUserModel = async (body) => {
 };
 
 const updateUserModel = async (body, id) => {
-    try {
-        const result = await pool.query(`UPDATE users SET name = $1, position = $2, domicile = $3, last_work = $4, description = $5, photo = $6, skill_name = $7 WHERE id = $8`, [body.name, body.position, body.domicile, body.last_work, body.description, body.photo, body.skill_name, id]);
+  try {
+    const result = await pool.query(`UPDATE users SET name = $1, position = $2, domicile = $3, last_work = $4, description = $5, photo = $6, skill_name = $7 WHERE id = $8`, [
+      body.name,
+      body.position,
+      body.domicile,
+      body.last_work,
+      body.description,
+      body.photo,
+      body.skill_name,
+      id,
+    ]);
 
-        return result;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
 const deleteUserModel = async (id) => {
-    try {
-        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [id]);
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [id]);
 
-        return result;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
 const loginModel = async (body) => {
   try {
     const loginUserQuerySql = 'SELECT * FROM users WHERE email = $1 AND password = $2';
     values = [email, password];
-  
+
     return pool.query(loginUserQuerySql, values);
   } catch (error) {
     throw new Error(error.message);
@@ -67,7 +89,7 @@ const activateUserModel = async (id) => {
   try {
     const activateUserQuerySql = 'UPDATE users SET verified=true WHERE id=$1';
     values = [id];
-  
+
     return pool.query(activateUserQuerySql, values);
   } catch (error) {
     throw new Error(error.message);
@@ -82,4 +104,5 @@ module.exports = {
   deleteUserModel,
   loginModel,
   activateUserModel,
+  GetallCandidateModel,
 };
--- src/route/UserRoute.js
+++ src/route/UserRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 const UserControllers = require('../controller/UserController');
-const {VertifikasiToken} = require('./../midlleware/VertifikasiToken');
+const { VertifikasiToken } = require('./../midlleware/VertifikasiToken');
 const { getPortPageViewController } = require('../controller/PortfolioViewController');
 
 route.post('/register', UserControllers.createUserController);
@@ -11,5 +11,6 @@ route.post('/update', VertifikasiToken, UserControllers.updateProfile);
 route.get('/in', VertifikasiToken, UserControllers.getUserByIdController);
 route.get('/portfolio-view/:id', VertifikasiToken, getPortPageViewController);
 route.get('/verify/:id', UserControllers.activateUserController);
+route.get('/allCandidate', VertifikasiToken, UserControllers.getAllUserController);
 
 module.exports = route;
